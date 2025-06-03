9a8f9f34e359e136b8e0c039b6760b2fe3309c66
Anshuman Rathore <88876420+AnshumanRathore@users.noreply.github.com>
2025-06-03 08:23:54 +0000

    lecture

--- Test01/test.js
+++ Test01/test.js
@@ -1,1 +1,9 @@
-console.log("ANSHUMAN")
+const accountId= 01
+// accountId=23
+let accEmail="lakha122@gmail.com"
+var accPassword="Munna12"
+accountCity="Mumbai"
+console.log(accountId);
+console.table([accEmail,accountId,accPassword,accountCity]);
+console.log(typeof undefined);
+console.log(typeof null);
