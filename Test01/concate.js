4538f15e1e2f5267eacd4ae6f35dfd4d214c7278
Anshuman Rathore <88876420+AnshumanRathore@users.noreply.github.com>
2025-06-05 08:20:32 +0000

    String

---
+++ Test01/concate.js
@@ -0,0 +1,12 @@
+const name="Anshuman"
+const company="S.K ltd."
+let count=50
+///CONCATENATION with `` of strings
+console.log(`Hello my name is ${name} chairman of ${company} with ${count} years.`)
+
+
+///STRING functions 
+const gameName=new String(`Anshu`)
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toLowerCase());
