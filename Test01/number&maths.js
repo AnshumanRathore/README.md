68d5699afc9044d42837e75f546c1f20e3104bd3
Anshuman Rathore <88876420+AnshumanRathore@users.noreply.github.com>
2025-06-06 18:08:31 +0000

    number & maths

--- Test01/number&maths.js
+++ Test01/number&maths.js
@@ -1,1 +1,9 @@
-console log
+const score=500
+console.log(score);
+
+const balance =new Number(100)
+console.log(balance);
+console.log(Number);
+console.log(balance.toFixed(3));
+console.log(balance.toString());
+
