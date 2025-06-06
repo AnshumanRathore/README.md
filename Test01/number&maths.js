ab193d0af8120c130742a0e7ab27cc5a5a6059e1
Anshuman Rathore <88876420+AnshumanRathore@users.noreply.github.com>
2025-06-06 18:22:51 +0000

    number/ maths

--- Test01/number&maths.js
+++ Test01/number&maths.js
@@ -7,3 +7,20 @@ console.log(Number);
 console.log(balance.toFixed(3));
 console.log(balance.toString());
 
+const othernum=3.14756
+console.log(othernum.toPrecision(4));
+
+const hundreds=1000000
+console.log(hundreds.toLocaleString('en-In'));
+
+
+//////MATHS
+console.log(Math);
+console.log(Math.abs(-988));
+console.log(Math.round(3.56));
+console.log(Math.ceil(3.56));
+console.log(Math.floor(3.56));
+console.log(Math.max(332,45,67,87,44,32,3.56));
+
+
+
