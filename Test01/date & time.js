a4f0909414bff2420c2d16eb20aade32b5c0ba97
Anshuman Rathore <88876420+AnshumanRathore@users.noreply.github.com>
2025-06-07 18:11:04 +0000

    DATE

--- Test01/date & time.js
+++ Test01/date & time.js
@@ -1,5 +1,8 @@
 let a= new Date()
 console.log(a);
 console.log(a.getMinutes());
-console.log(a.t);
+console.log(a.toLocaleDateString());
+console.log(a.toUTCString());
+console.log(a.toLocaleString());
+
 
