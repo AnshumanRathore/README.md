8304c638cf90e5f249546453ee919b436ccb2d9a
Anshuman Rathore <88876420+AnshumanRathore@users.noreply.github.com>
2025-06-08 18:08:00 +0000

    arrays

--- Part2/array.js
+++ Part2/array.js
@@ -3,3 +3,13 @@ console.log("Slice",arr1.slice(2,4));
 
 console.log("Splice",arr1.splice(2,4));
 console.log("Array Left",arr1);
+
+
+const arr2=[9,8,7,6,5,4,3]
+console.log("Arr2",arr2);
+// console.log("Arr2",arr2.unshift(88));
+arr2.unshift(990);
+console.log("Arr2 after unshift :",arr2);
+ 
+arr2.shift();
+console.log("Arr2 after shift :",arr2);
