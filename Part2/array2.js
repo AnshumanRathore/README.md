4e3ec776c256d452d92f8fad1888a2f2e79547df
Anshuman Rathore <88876420+AnshumanRathore@users.noreply.github.com>
2025-06-08 18:26:26 +0000

    array concat

--- Part2/array2.js
+++ Part2/array2.js
@@ -2,7 +2,10 @@ const arr1=["mukesh","geo","lunes"]
 
 const arr2=["martes","domingo","cinco"]
 
-arr1.push(arr2);
-console.log(arr1);
+// arr1.push(arr2);
 
-console.log("ACCESS", arr1[3][1]);
+// console.log("ACCESS", arr1[3][1]);
+
+const both_arr=arr1.concat(arr2);
+console.log("Concat",both_arr);
+
