8ba7600ba6fb1ad251946922354ccad0bc456ef5
Anshuman Rathore <88876420+AnshumanRathore@users.noreply.github.com>
2025-06-09 18:29:04 +0000

    object-symbol

--- Part2/objects1.js
+++ Part2/objects1.js
@@ -1,5 +1,9 @@
+//Symbol datatype (Primitive values)
+const mysym =Symbol("key1")
+
 //object literals
 const user= {
+    [mysym]:" Symbol value",
     name : "anshu",
     umar: 20,
     location: "jaipur",
@@ -11,4 +15,6 @@ console.log(user.name);
 console.log(user["location"]);
 console.log(user.days);
 
-//Symbol datatype (Primitive values)
+//symbol is written or use by using []
+console.log(user[mysym]);
+
